be3e3cd67c24fa81fcd990382581dd3b05cc7d2d
ranxuhui33@163.com <ranxuhui33@163.com>
2018-05-30 20:44:00 +0800

    4.1

--- car_home/spiders/auto_spider.py
+++ car_home/spiders/auto_spider.py
@@ -17,23 +17,27 @@ class AutoSpiderSpider(scrapy.Spider):
         return reqs
 
     def parse(self, response):
-        Car_Calss_Web = response.xpath('//div[@class="list"]')  # 选取所有所有div元素，且这些元素拥有值为homepage-hotcar的class属性
 
-        Car_Top = Car_Calss_Web.xpath('div[@class="name"]')
+        # hotcar-content → hatcar-n → list → name（车类型） → box(具体车名)
 
-        Car_Name = Car_Calss_Web.xpath('div[2]/div/ul/li/div/p[@data-gcjid]')
+        Car_Calss_Web_fix = response.xpath('//div[@class="hotcar-content"]')  # 选取所有所有div元素，且这些元素拥有值为homepage-hotcar的class属性
+        Car_Hotcar = Car_Calss_Web_fix.xpath('div[1]/div[1]')                 # 对应<div class="list">
+        Car_Luxury = Car_Calss_Web_fix.xpath('div[2]/div[1]')
 
-        for Car_Class in Car_Top:
-            item = CarHomeItem()
+        for Car_Class in Car_Hotcar:
+            Car_Name = Car_Calss_Web_fix.xpath('div[1]/div[1]/div[2]/div/ul/li/div/p[@data-gcjid]') # 具体到车的名字
             for Car in Car_Name:
-                item['Car_Class'] = Car_Class.xpath('string(a)')[0].extract()
                 item = CarHomeItem()
+                # 车名
                 item['Car_Name'] = Car.xpath('string(a)')[0].extract()
+                # 车的类型
+                # item['Car_Class'] = Car_Class.xpath('div[1]/text()')[0].extract() bug
+                item['Car_Class'] = Car_Class.xpath('div[1]/a/text()')[0].extract()  # item['Car_Class'] = Car_Class.xpath('div[1]/text()')[0].extract() bug
                 Car_url_1 = Car.xpath('a').re('(?<=\/)\d+(?=\/)')      # 注意这里的Car_url_1是一个列表
                 Car_url_2 = Car.xpath('a').re('=\d+')
                 item['Car_url'] = Car_url_1 if 'https:' in Car_url_1 else ('https://www.autohome.com.cn/' + Car_url_1[0] + '/#pvareaid' +Car_url_2[0])
-
                 yield scrapy.Request(url=item['Car_url'], meta={'item': item}, callback=self.parse_detail, dont_filter=True)
+
 
     def parse_detail(self, response):
         item = response.meta['item']
