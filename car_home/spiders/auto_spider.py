c7b5ac7869b86f28811615f1b18d6de3c289635d
ranxuhui33@163.com <ranxuhui33@163.com>
2018-05-31 14:00:39 +0800

    2018/5/31/14:00

--- car_home/spiders/auto_spider.py
+++ car_home/spiders/auto_spider.py
@@ -10,8 +10,6 @@ class AutoSpiderSpider(scrapy.Spider):
 
     def start_requests(self):
         reqs = []
-
-        # req = scrapy.Request("http://www.xicidaili.com/nn/%s"%i)
         req = scrapy.Request("https://www.autohome.com.cn/beijing/")
         reqs.append(req)
         return reqs
@@ -19,26 +17,40 @@ class AutoSpiderSpider(scrapy.Spider):
     def parse(self, response):
 
         # hotcar-content → hatcar-n → list → name（车类型） → box(具体车名)
-
         Car_Calss_Web_fix = response.xpath('//div[@class="hotcar-content"]')  # 选取所有所有div元素，且这些元素拥有值为homepage-hotcar的class属性
         Car_Hotcar = Car_Calss_Web_fix.xpath('div[1]/div[1]')   # 第二个div对应              # 对应<div class="list">
 
         # 热门车之外的豪华车、小型/其他....是动态界面
         # Car_Luxury = Car_Calss_Web_fix.xpath('div[2]/div[1]')
 
-        for Car_Class in Car_Hotcar:
-            Car_Name = Car_Calss_Web_fix.xpath('div[1]/div[1]/div[2]/div/ul/li/div/p[@data-gcjid]') # 第二个div对应    # 具体到车的名字
-            for Car in Car_Name:
-                item = CarHomeItem()
-                # 车名
-                item['Car_Name'] = Car.xpath('string(a)')[0].extract()
+        Car_Name = Car_Calss_Web_fix.xpath('div[1]/div/div[2]/div/ul/li/div/p[@data-gcjid]') # 第二个div对应    # 具体到车的名字
+        i = 0
+        for Car in Car_Name:
+            item = CarHomeItem()
+            # 车名
+            item['Car_Name'] = Car.xpath('string(a)')[0].extract()
+
+            if i < 25:
                 # 车的类型
                 # item['Car_Class'] = Car_Class.xpath('div[1]/text()')[0].extract() bug
-                item['Car_Class'] = Car_Class.xpath('div[1]/a/text()')[0].extract()  # item['Car_Class'] = Car_Class.xpath('div[1]/text()')[0].extract() bug
-                Car_url_1 = Car.xpath('a').re('(?<=\/)\d+(?=\/)')      # 注意这里的Car_url_1是一个列表
-                Car_url_2 = Car.xpath('a').re('=\d+')
-                item['Car_url'] = Car_url_1 if 'https:' in Car_url_1 else ('https://www.autohome.com.cn/' + Car_url_1[0] + '/#pvareaid' +Car_url_2[0])
-                yield scrapy.Request(url=item['Car_url'], meta={'item': item}, callback=self.parse_detail, dont_filter=True)
+                item['Car_Class'] = Car_Calss_Web_fix.xpath('div[1]/div[1]/div/a/text()')[0].extract()  # item['Car_Class'] = Car_Class.xpath('div[1]/text()')[0].extract() bug
+
+            elif 24 < i < 49:
+                # 车的类型
+                # item['Car_Class'] = Car_Class.xpath('div[1]/text()')[0].extract() bug
+                item['Car_Class'] = Car_Calss_Web_fix.xpath('div[1]/div[2]/div/a/text()')[0].extract()  # item['Car_Class'] = Car_Class.xpath('div[1]/text()')[0].extract() bug
+
+            elif 48 < i < 75:
+                # 车的类型
+                # item['Car_Class'] = Car_Class.xpath('div[1]/text()')[0].extract() bug
+                item['Car_Class'] = Car_Calss_Web_fix.xpath('div[1]/div[3]/div/a/text()')[0].extract()  # item['Car_Class'] = Car_Class.xpath('div[1]/text()')[0].extract() bug
+
+            i += 1
+
+            Car_url_1 = Car.xpath('a').re('(?<=\/)\d+(?=\/)')      # 注意这里的Car_url_1是一个列表
+            Car_url_2 = Car.xpath('a').re('=\d+')
+            item['Car_url'] = Car_url_1 if 'https:' in Car_url_1 else ('https://www.autohome.com.cn/' + Car_url_1[0] + '/#pvareaid' +Car_url_2[0])
+            yield scrapy.Request(url=item['Car_url'], meta={'item': item}, callback=self.parse_detail, dont_filter=True)
 
 
     def parse_detail(self, response):
