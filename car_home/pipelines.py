112e2cb4af5bfae3afd400244fedd45b3a91e429
ranxuhui33@163.com <ranxuhui33@163.com>
2018-06-07 10:05:07 +0800

    顺利导入数据库

--- car_home/pipelines.py
+++ car_home/pipelines.py
@@ -31,36 +31,35 @@ class CarHome_INFO_Pipeline(object):
 class CarHomeSQLPipeline(object):
     def __init__(self):
         # 刚开始时连接数据库
-        self.connect = pymysql.connect(host='127.0.0.1', user='root', passwd='root', db='car_home')
+        self.connect = pymysql.connect(host='127.0.0.1', user='root', passwd='root',
+                                       db='car_home', charset='utf8', use_unicode=True)
 
         # 通过cursor执行增删查改
         self.cursor = self.connect.cursor()
 
     def process_item(self, item, spider):
 
-        # 插入数据
-        for i in range(len(item["Car_Name"])):
-            Name = item["Car_Name"][i]
-            for j in range(len(item["Car_Color"][i])):
-                Color = item["Car_Color"][j]
-                self.cursor.execute("insert into info(Car_Color)VALUES ('" + Color + "')")
-            self.cursor.execute("insert into info(Car_Name)VALUES ('"+Name+"')")
+        try:
+            # 插入数据
+            self.cursor.execute(
+                """insert into info(Car_Name, Car_Class, Car_url, New_car_guide ,Car_Engine, Speed_Changing_Box, Body_structure, Car_Color)
+                value (%s, %s, %s, %s, %s, %s, %s, %s)""",
+                (item['Car_Name'],
+                 item['Car_Class'],
+                 item['Car_url'],
+                 item['New_car_guide'],
+                 item['Engine'],
+                 item['Speed_Changing_Box'],
+                 item['Body_structure'],
+                 item['Car_Color']
+            ))
 
+            # 提交sql语句
+            self.connect.commit()
 
-
-        # Class = item["Car_Class"][1]
-        # Url = item["Car_url"][1]
-        # New = item["New_car_guide"][1]
-        # Engine = item["Engine"][1]
-        # Speed = item["Speed_Changing_Box"][1]
-        # Body = item["Body_structure"][1]
-
-
-        # self.cursor.execute("insert into info(Car_Name, Car_Class, Car_url, New_car_guide,"
-        #                     " Car_Engine, Speed_Changing_Box, Body_structure, Car_Color)"
-        #                     "VALUES('"+Name+"', '"+Class+"', '"+Url+"', '"+New+"', '"+Engine+"', '"+Speed+"', '"+Body+"' ,'"+Color+"')")
-
-        # 提交sql语句
+        except Exception as error:
+            # 出现错误时打印错误日志
+            log(error)
             self.connect.commit()
         return item
 
--- car_home/spiders/auto_spider.py
+++ car_home/spiders/auto_spider.py
@@ -28,22 +28,22 @@ class AutoSpiderSpider(scrapy.Spider):
         for Car in Car_Name:
             item = CarHomeItem()
             # 车名
-            item['Car_Name'] = Car.xpath('string(a)').extract()
+            item['Car_Name'] = Car.xpath('string(a)').extract()[0]
 
             if i < 25:
                 # 车的类型
                 # item['Car_Class'] = Car_Class.xpath('div[1]/text()')[0].extract() bug
-                item['Car_Class'] = Car_Calss_Web.xpath('div[1]/div[1]/div[1]/a/text()').extract()  # item['Car_Class'] = Car_Class.xpath('div[1]/text()')[0].extract() bug
+                item['Car_Class'] = Car_Calss_Web.xpath('div[1]/div[1]/div[1]/a/text()').extract()[0]  # item['Car_Class'] = Car_Class.xpath('div[1]/text()')[0].extract() bug
 
             elif 24 < i < 49:
                 # 车的类型
                 # item['Car_Class'] = Car_Class.xpath('div[1]/text()')[0].extract() bug
-                item['Car_Class'] = Car_Calss_Web.xpath('div[1]/div[2]/div[1]/a/text()').extract()  # item['Car_Class'] = Car_Class.xpath('div[1]/text()')[0].extract() bug
+                item['Car_Class'] = Car_Calss_Web.xpath('div[1]/div[2]/div[1]/a/text()').extract()[0]  # item['Car_Class'] = Car_Class.xpath('div[1]/text()')[0].extract() bug
 
             elif 48 < i < 75:
                 # 车的类型
                 # item['Car_Class'] = Car_Class.xpath('div[1]/text()')[0].extract() bug
-                item['Car_Class'] = Car_Calss_Web.xpath('div[1]/div[3]/div[1]/a/text()').extract()  # item['Car_Class'] = Car_Class.xpath('div[1]/text()')[0].extract() bug
+                item['Car_Class'] = Car_Calss_Web.xpath('div[1]/div[3]/div[1]/a/text()').extract()[0]  # item['Car_Class'] = Car_Class.xpath('div[1]/text()')[0].extract() bug
 
             i += 1
 
@@ -57,21 +57,40 @@ class AutoSpiderSpider(scrapy.Spider):
     def parse_detail(self, response):
         item = response.meta['item']
         # 新车指导价
-        item['New_car_guide'] = response.xpath('/html/body/div[2]/div[4]/div[1]/div[1]/div[1]/div[2]/dl/dt[1]/a[1]/text()').extract()
+        item['New_car_guide'] = response.xpath('/html/body/div[2]/div[4]/div[1]/div[1]/div[1]/div[2]/dl/dt[1]/a[1]/text()').extract()[0]
 
         # 下面两项车商城报价和二手车报价是js/ajax动态生成的数据使用上面的方法无法爬取
         # item['Mall_price'] = response.xpath('/html/body/div[2]/div[4]/div[1]/div[1]/div[1]/div[2]/dl/dt[2]/a[1]/text()')[0].extract()
         # item['Secondhand_price'] = response.xpath('/html/body/div[2]/div[4]/div[1]/div[1]/div[1]/div[2]/dl/dt[3]/a[1]/text()')[0].extract()
 
-        item['Engine'] = response.xpath('/html/body/div[2]/div[4]/div[1]/div[1]/div[1]/div[2]/dl/dd[2]/a/text()').extract()
+        # item['Engine'] = response.xpath('/html/body/div[2]/div[4]/div[1]/div[1]/div[1]/div[2]/dl/dd[2]/a/text()').extract()
+        engines = response.xpath('/html/body/div[2]/div[4]/div[1]/div[1]/div[1]/div[2]/dl/dd[2]/a/text()').extract()
+        Engine = ''
+        for i in range(len(engines)):
+            Engine += engines[i] + ','
+        item['Engine'] = Engine
 
         # a[0:-1]是变速箱属性
-        item['Speed_Changing_Box'] = response.xpath('/html/body/div[2]/div[4]/div[1]/div[1]/div[1]/div[2]/dl/dd[3]/a/text()')[:-1].extract()
+        # item['Speed_Changing_Box'] = response.xpath('/html/body/div[2]/div[4]/div[1]/div[1]/div[1]/div[2]/dl/dd[3]/a/text()').extract()[:-1]
+        Speeds = response.xpath('/html/body/div[2]/div[4]/div[1]/div[1]/div[1]/div[2]/dl/dd[3]/a/text()').extract()[:-1]
+        Speed = ''
+        for i in range(len(Speeds)):
+            Speed += Speeds[i] + ','
+        item['Speed_Changing_Box'] = Speed
 
         # a[-1]是车体结构
-        item['Body_structure'] = response.xpath('/html/body/div[2]/div[4]/div[1]/div[1]/div[1]/div[2]/dl/dd[3]/a/text()')[-1].extract()
+        # item['Body_structure'] = response.xpath('/html/body/div[2]/div[4]/div[1]/div[1]/div[1]/div[2]/dl/dd[3]/a/text()')[-1].extract()
+        bodies = response.xpath('/html/body/div[2]/div[4]/div[1]/div[1]/div[1]/div[2]/dl/dd[3]/a/text()')[-1].extract()
+        Body = ''
+        for i in range(len(bodies)):
+            Body += bodies[i]
+        item['Body_structure'] = Body
 
-        item['Car_Color'] = response.xpath('/html/body/div[2]/div[4]/div[1]/div[1]/div[1]/div[2]/dl/dd[1]/div/a/div/div/text()').extract()
+        colors =response.xpath('/html/body/div[2]/div[4]/div[1]/div[1]/div[1]/div[2]/dl/dd[1]/div/a/div/div/text()').extract()
+        Car_Color = ''
+        for i in range(len(colors)):
+            Car_Color += colors[i] + ','
+        item['Car_Color'] = Car_Color
 
         yield item
 
